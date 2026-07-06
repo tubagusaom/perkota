b5b1787b696915e9de1f1d75c2ce92212467b57e
Tera Byte <tubagus.aom.swk@gmail.com>
2026-07-06 16:15:05 +0700

    mergeupdate

--- application/views/templates/pendaftaran/qr_pay.php
+++ application/views/templates/pendaftaran/qr_pay.php
@@ -1,40 +1,40 @@
 <style>
     body{
-  width:100%;
-  height:auto;
-  background-color:hsl(212, 45%, 89%);
-  max-width:1440px;
-  min-width:3.75vw;
-}
-.box{
-  background-color: hsl(0, 0%, 100%);
-  width:280px;
-  height:440px;
-  
-  border-radius: 20px;
-  margin:5% auto;
-
-}
-.img{
-  width:90%;
-margin:15px;  border-radius: 10px;
-}
-h1{
-  font-family: 'Hanken Grotesk', sans-serif;
-  color:hsl(218, 44%, 22%);
-  font-weight: 1000;
-  text-align: center;
-  font-size:20px;
-  margin:auto 10px;
-}
-p{
-  font-family: 'Hanken Grotesk', sans-serif;
-  font-size: 14px;
-  color: grey;
-  font-weight: 400;
-  text-align: center;
-  margin:12px 20px 10px 20px;
-}
+        width:100%;
+        height:auto;
+        background-color:hsl(212, 45%, 89%);
+        max-width:1440px;
+        min-width:3.75vw;
+    }
+    .box{
+        background-color: hsl(0, 0%, 100%);
+        width:450px;
+        height:570px;
+        
+        border-radius: 20px;
+        /* margin:5% auto; */
+    }
+    .img{
+        width:90%;
+        margin:22px;
+        border-radius: 10px;
+    }
+    h1{
+        font-family: 'Hanken Grotesk', sans-serif;
+        color:hsl(218, 44%, 22%);
+        font-weight: 1000;
+        text-align: center;
+        font-size:20px;
+        margin:auto 10px;
+    }
+    p{
+        font-family: 'Hanken Grotesk', sans-serif;
+        font-size: 14px;
+        color: grey;
+        font-weight: 400;
+        text-align: center;
+        margin:12px 20px 10px 20px;
+    }
 </style>
 
 <div class="box">
